b9a2f14386179779af6be6305615a9d4ccfaf80f
Julian Ocare <ocarejulian@gmail.com>
2022-10-06 00:15:39 -0300

    correcion en funciones

--- JS/script.js
+++ JS/script.js
@@ -14,24 +14,24 @@ console.log(suma(num1, num2));
 
 //#region
 function resta(num1, num2) {
-    let s = num1 - num2;
-    return s;
+    let r = num1 - num2;
+    return r;
 }
 console.log(resta(num1, num2));
 //#endregion
 
 //#region
 function division(num1, num2) {
-    let s = num1 / num2;
-    return s;
+    let d = num1 / num2;
+    return d;
 }
 console.log(division(num1, num2));
 //#endregion
 
 //#region
 function multiplicacion(num1, num2) {
-    let s = num1 * num2;
-    return s;
+    let m = num1 * num2;
+    return m;
 }
 console.log(multiplicacion(num1, num2));
 //#endregion
