478845b61d699b81ba5d03fb12c4e210ae606e91
Julian Ocare <ocarejulian@gmail.com>
2022-10-05 23:47:55 -0300

    Funcion resta

--- JS/script.js
+++ JS/script.js
@@ -11,3 +11,11 @@ function suma(num1, num2) {
 }
 consologe.log(suma(num1, num2));
 //#endregion
+
+//#region
+function resta(num1, num2) {
+    let s = num1 - num2;
+    return s;
+}
+consologe.log(resta(num1, num2));
+//#endregion
