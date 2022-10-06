504081cca19e9f8e7bf526d8e6ce748155901c83
Julian Ocare <ocarejulian@gmail.com>
2022-10-05 23:39:06 -0300

    Funcion suma.script

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,11 @@
 var nombre = "julian";
 let apellido = 'ocare';
 //#endregion
+
+//#region
+function suma(num1, num2) {
+    let s = num1 + num2;
+    return s;
+}
+consologe.log(suma(num1, num2));
+//#endregion
